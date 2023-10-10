bbc1dd7d68aec5ec83cb29216da4499128023d58
Dragan Balatinac <balatinac@cp-compartner.de>
2023-10-10 13:46:13 +0200

    Add to the post deploy hook

--- packages/deployment/enviroments/base.js
+++ packages/deployment/enviroments/base.js
@@ -2,5 +2,5 @@ module.exports = {
     "user": "root",
     "host": "168.119.253.96",
     "repo": "git@github.com:balatD/prattle.git",
-    "post-deploy": "npm install && npm run build"
+    "post-deploy": "nvm use 18 && npm install && npm run build"
 };
