8cc03def93664d313f29dfb752f0504d2b956831
Dragan Balatinac <balatinac@cp-compartner.de>
2023-10-10 16:41:52 +0200

    Test out new pre deploy settings

--- packages/deployment/hooks/preDeploy.js
+++ packages/deployment/hooks/preDeploy.js
@@ -2,9 +2,9 @@ const hookUtility = require("./hookUtility");
 
 const commands = [
     // Creates a .env if not present in source folder
-    "[ ! -h .env ] && ln -s ../shared/.env .env",
-    "[ ! -h apps/backend/.env ] && ln -s ../../.env apps/backend/.env",
-    "[ ! -h apps/frontend/.env ] && ln -s ../../.env apps/frontend/.env"
+    "[ -h .env ] || ln -s ../shared/.env .env",
+    "[ -h apps/backend/.env ] || ln -s ../../.env apps/backend/.env",
+    "[ -h apps/frontend/.env ] || ln -s ../../.env apps/frontend/.env"
 ];
 
 module.exports = hookUtility.createCommandString(commands);
