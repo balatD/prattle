d759a7e9eb6d2236dc06d8bb3fa3cfff0203a553
Dragan Balatinac <balatinac@cp-compartner.de>
2023-10-10 16:32:19 +0200

    Fix predeploy hook

--- packages/deployment/hooks/preDeploy.js
+++ packages/deployment/hooks/preDeploy.js
@@ -3,8 +3,8 @@ const hookUtility = require("./hookUtility");
 const commands = [
     // Creates a .env if not present in source folder
     "[ -f .env ] || ln -s ../shared/.env .env",
-    "[ -f apps/backend/.env ] || ln -s .env apps/backend/.env",
-    "[ -f apps/frontend/.env ] || ln -s .env apps/frontend/.env"
+    "[ -f apps/backend/.env ] || ln -s ../../.env apps/backend/.env",
+    "[ -f apps/frontend/.env ] || ln -s ../../.env apps/frontend/.env"
 ];
 
 module.exports = hookUtility.createCommandString(commands);
