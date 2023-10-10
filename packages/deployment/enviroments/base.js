792718cfeb2fdcf47d87d4fea6526a6d31b96b3c
Dragan Balatinac <balatinac@cp-compartner.de>
2023-10-10 13:58:55 +0200

    Change hook

--- packages/deployment/enviroments/base.js
+++ packages/deployment/enviroments/base.js
@@ -2,5 +2,5 @@ module.exports = {
     "user": "root",
     "host": "168.119.253.96",
     "repo": "git@github.com:balatD/prattle.git",
-    "post-deploy": ". ~/.bashrc && npm install && npm run build"
+    "post-deploy": ". ~/.bashrc && npm install && npm run start"
 };
