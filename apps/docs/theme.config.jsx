e1bf84825d61b4c608d1a1314355df44ba825201
Dragan Balatinac <balatinac@cp-compartner.de>
2023-11-04 16:17:00 +0100

    Add nextra as docs theme / generator

---
+++ apps/docs/theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+    logo: <span>Prattle Docs</span>,
+    project: {
+        link: 'https://github.com/balatD/prattle'
+    },
+    docsRepositoryBase: 'https://github.com/balatD/prattle/tree/main/apps/docs'
+}
